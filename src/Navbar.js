009c4fdb26fff4638072f3e0ae72a074f77b122d
ParthGupta-28 <pg6272695@gmail.com>
2023-01-17 16:14:40 +0530

    dark mode

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,24 @@
+import { IconButton, Spacer, useColorMode } from "@chakra-ui/react";
 import React from "react";
 import logo from "./images/Troll Face.png";
 import "./Navbar.css";
+import { FaSun, FaMoon } from "react-icons/fa";
 
 export default function Navbar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <div className="header">
       <img src={logo} className="header-logo" />
       <h2 className="header-text">Meme Generator</h2>
+      <Spacer />
+      <IconButton
+        onClick={toggleColorMode}
+        icon={colorMode === "light" ? <FaSun /> : <FaMoon />}
+        isRound={true}
+        size="lg"
+        p={4}
+      />
     </div>
   );
 }
